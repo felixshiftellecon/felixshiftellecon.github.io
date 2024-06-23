25fabb1aee92fd79a1b5eba35731b436da1fd153
Nick Smith <tubaice@gmail.com>
2024-06-23 17:22:23 -0400

    Revert "about us done"
    This reverts commit 5267524a1ea5332c99da4d25375bd7e1153d9b59.

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,8 +1,26 @@
-import { linkStyle } from 'components/link-style/link_style';
+import type { Metadata } from 'next';
+
+import ContactForm from 'components/contact-form';
 import Prose from 'components/prose';
 import { getPage } from 'lib/shopify';
-import Image from 'next/image';
 import { notFound } from 'next/navigation';
+
+export async function generateMetadata(): Promise<Metadata> {
+  const metafields = [{ key: 'multiple_text_contact', namespace: 'panini-cake' }];
+  const page = await getPage('contact', metafields);
+
+  if (!page) return notFound();
+
+  return {
+    title: page.seo?.title || page.title,
+    description: page.seo?.description || page.bodySummary,
+    openGraph: {
+      publishedTime: page.createdAt,
+      modifiedTime: page.updatedAt,
+      type: 'article'
+    }
+  };
+}
 
 export default async function ContactPage() {
   const metafields = [{ key: 'multiple_text_contact', namespace: 'panini-cake' }];
@@ -11,63 +29,54 @@ export default async function ContactPage() {
   if (!page) return notFound();
 
   const GoogleMapEmbed = () => (
-    <div className="mb-[70px] pb-10 text-center leading-7">
-      <p> Two naturalists spreading delicious food, knowledge, and love</p>
-      <div className="pb-10 pt-10">
-        <Image
-          src="https://i.postimg.cc/bJXM7GVT/IMG-20240622-212849.jpg"
-          width={1000}
-          height={1120}
-          alt="bee loading"
-          className=""
-          priority
-        />
-      </div>
-      <h2 className="mb-8 text-header-2 font-semibold leading-none">Come on by!</h2>
+    <div className="mb-[70px] leading-7">
+      <h2 className="mb-8 text-header-2 font-semibold leading-none">Find us</h2>
       <p className="leading-7">
-        Want to buy something or checkout our farm?
-        <br></br>
-        We're happy to meet you!
-        <br></br>
-        Feel free to give us a call at{' '}
-        <a style={linkStyle} href="tel:2404068117">
-          (240) 406-8117
-        </a>
+        At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium
+        voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati
+        cupiditate non provident.
       </p>
       <iframe
-        src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d12295.044877868326!2d-77.9814276!3d39.6100611!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89ca1fddc8a70dd5%3A0x460dfd645b246cd4!2sSugar%20Snap%20Pea%20Farm!5e0!3m2!1sen!2sus!4v1719174991713!5m2!1sen!2sus"
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d190028.35588931548!2d12.37084718518173!3d41.91020830050026!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x132f6196f9928ebb%3A0xb90f770693656e38!2sRome%2C%20Metropolitan%20City%20of%20Rome%20Capital%2C%20Italy!5e0!3m2!1sen!2suk!4v1716289994249!5m2!1sen!2suk"
         width="100%"
         height="450"
         style={{ border: 0 }}
         allowFullScreen={false}
         loading="lazy"
         referrerPolicy="no-referrer-when-downgrade"
-        className="mt-2.5 pb-10 pt-10"
+        className="mt-2.5"
       ></iframe>
       <h3 className="mt-2.5 text-header-3 font-semibold">Our location</h3>
-      <div className="mt-2.5 leading-tight text-dark-teal">
-        <a href="https://maps.app.goo.gl/2qcezohCNfxEAAYg9">
-          <p>10864 Garrison Hollow Rd</p>
-          <p>Clear Spring, Maryland</p>
-          <p>21722</p>
-        </a>
-        <br></br>
-        <p>
-          <a href="tel:2404068117">(240) 406-8117</a>
-        </p>
-        <br></br>
-        <p>
-          <a href="mailto:sugarsnappeafarm@gmail.com">sugarsnappeafarm@gmail.com</a>
-        </p>
-      </div>
+      <p>12190 Beahan Street 59025</p>
+      <p>West Virginia</p>
+      <p>(979) 541-3809</p>
+      <p>info@hello.com</p>
     </div>
   );
 
   return (
     <>
-      <h1 className="mb-8 text-center text-5xl font-bold">About Us</h1>
+      <h1 className="mb-8 text-5xl font-bold">{page.title}</h1>
       <Prose className="mb-[70px]" html={page.body as string} />
       <GoogleMapEmbed />
+      <ContactForm />
+      {page.metafields &&
+        page.metafields?.filter((metafield) => metafield?.key === 'multiple_text_contact').length >
+          0 && (
+          <>
+            <h2 className="mb-8 text-header-2 font-semibold leading-none">
+              Something else from metafields
+            </h2>
+            <Prose
+              className="mb-[70px]"
+              html={
+                page.metafields?.filter(
+                  (metafield) => metafield?.key === 'multiple_text_contact'
+                )[0]?.value as string
+              }
+            />
+          </>
+        )}
     </>
   );
 }
