3eafd5fa2352d37cf1c5046e85e66cb6e337b634
ns-op <ns@efficient-engineering.com>
2025-05-03 13:20:04 -0400

    fix formatting, fix header formatting

--- app/about/layout.tsx
+++ app/about/layout.tsx
@@ -3,7 +3,7 @@ import Header from 'components/layout/header';
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <>
-      <Header isInternalPage />
+      <Header isInternalPage={false} />
       <div className="w-full">
         <div className="container mx-auto  max-w-2xl px-2.5">{children}</div>
       </div>
